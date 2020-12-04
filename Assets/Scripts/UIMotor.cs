5d796f515acca9cfb44909c81e9ace6c44cd2468
Manuel Duro <mduro@edu.tecnocampus.cat>
2020-12-04 20:12:37 +0100

    MedioMario

---
+++ Assets/Scripts/UIMotor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMotor : MonoBehaviour
+{
+    Animator uiAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            uiAnim.SetBool("show", true);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            uiAnim.SetBool("show", false);
+        }
+    }
+}
---
+++ Assets/Scripts/WallJump.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallJump : MonoBehaviour
+{
+    PlayerController playerController;
+    [SerializeField] float horizontalSpeed = 10f;
+    [SerializeField] float verticalSpeed = 150f;
+    [SerializeField] float dashTime = 1f;
+    [SerializeField] LayerMask layerMask;
+
+    [SerializeField] float wallThreshold=0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isWalled() && !playerController.isGrounded())
+        {
+            StartCoroutine(Jump());
+        }
+        
+    }
+
+    private bool isWalled()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        return Physics.Raycast(ray, out RaycastHit hit, wallThreshold, layerMask);
+    }
+
+    IEnumerator Jump()
+    {
+        float time = dashTime;
+        playerController.doWallJump(verticalSpeed);
+        playerController.activateMove(false);
+        transform.forward = -transform.forward;
+        while (time > 0)
+        {
+            time -= Time.deltaTime;
+            playerController.characterController.Move(transform.forward * horizontalSpeed * Time.deltaTime);
+
+            yield return null;
+        }
+        playerController.activateMove(true);
+    }
+}
