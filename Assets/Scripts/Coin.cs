d441efcb344b42aaaa1104bce3f68be2c7e99bfb
alcoru <55741940+alcoru@users.noreply.github.com>
2020-12-06 19:19:35 +0100

    HUD done

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,10 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Coin : MonoBehaviour
 {
     [SerializeField] Score score;
+    private Text textScore;
+
+    void Awake()
+    {
+        textScore = GameObject.FindGameObjectWithTag("ScoreText").GetComponent<Text>();
+    }
     private void OnTriggerEnter(Collider other) {
         if (score != null)
             score.score();
