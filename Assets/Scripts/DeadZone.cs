2040e2c14b044017bace5c19b8c4c34a6b6db813
Manuel Duro <mduro@edu.tecnocampus.cat>
2020-12-06 19:53:32 +0100

    Casi final

---
+++ Assets/Scripts/DeadZone.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadZone : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("HEEEEY");
+            GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameOverSystem>().GameOver();
+        }
+    }
+}
