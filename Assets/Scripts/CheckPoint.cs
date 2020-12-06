4979407b93893db4b25523db6cd04321bfe270f8
Manuel Duro <mduro@edu.tecnocampus.cat>
2020-12-06 21:16:35 +0100

    Fin

---
+++ Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<ResetTransform>(out ResetTransform resetTransform))
+        {
+            resetTransform.SetCurrentPositionTransform(transform);
+        }
+    }
+}
--- Assets/Scripts/ResetTransform.cs
+++ Assets/Scripts/ResetTransform.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class ResetTransform : MonoBehaviour, IRestartGame
 {
-   [SerializeField] Transform initialTransform;
+   Transform currentPositionTransform;
 
     Vector3 initPos;
     Quaternion initRot;
     public void Restart()
     {
         gameObject.SetActive(false);
-        if (initialTransform != null)
+        if (currentPositionTransform != null)
         {
-            transform.position = initialTransform.position;
-            transform.rotation = initialTransform.rotation;
+            transform.position = currentPositionTransform.position;
+            transform.rotation = currentPositionTransform.rotation;
         }
         else
         {
@@ -23,16 +23,6 @@ public class ResetTransform : MonoBehaviour, IRestartGame
         }
         gameObject.SetActive(true);
     }
-
-    // Start is called before the first frame update
-    /*void Start()
-    {
-        //GameManager.getInstance().addRestartGameObject(this);
-        GameManager.RestartGameEvent += Restart;
-        initPos = transform.position;
-        initRot = transform.rotation;
-
-    }*/
 
 
     private void OnDisable()
@@ -45,5 +35,10 @@ public class ResetTransform : MonoBehaviour, IRestartGame
         GameManager.RestartGameEvent += Restart;
         initPos = transform.position;
         initRot = transform.rotation;
+    }
+
+    public void SetCurrentPositionTransform(Transform transform)
+    {
+        currentPositionTransform = transform;
     }
 }
